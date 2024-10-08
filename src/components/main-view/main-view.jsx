6e5e051688bfb903b1e6c1df74f0e06ad82b6833
Jelloko <rainbow.samp@gmail.com>
2024-10-07 20:49:10 -0400

    This commit should import the Loginview and Signupview awell as create a logout button. This commit should also allow data to be stored in localStorage.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,15 +1,29 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser? storedUser : null);
+  const [token, setToken] = useState(storedToken? storedToken : null);
   const [movies, setMovies] = useState([]);
-
   const [selectedMovie, setSelectedMovie] = useState(null);
   useEffect(() => {
-    fetch("https://my-flix-cf-fd6a3633859c.herokuapp.com/movies")
+    if (!token) {
+      return;
+    }
+    fetch("https://my-flix-cf-fd6a3633859c.herokuapp.com/movies",{
+      headers: { Authorization: `Bearer ${token}` }
+    })
+    
       .then((response) => response.json())
       .then((data) => {
+        console.log(data);
+      });
+    }, [token]);
         const moviesFromApi = data.map((movie) => {
           return {
             _id: movie._id,
@@ -21,8 +35,19 @@ export const MainView = () => {
         });
 
         setMovies(moviesFromApi);
-      });
-  }, []);
+
+        if (!user) {
+          return (
+            <>
+              <LoginView onLoggedIn={(user, token) => {
+                setUser(user);
+                setToken(token);
+              }} />
+              or
+              <SignupView />
+            </>
+          );
+        }
 
   if (selectedMovie) {
     return (
@@ -36,6 +61,8 @@ export const MainView = () => {
   if (movies.length === 0) {
     return <div>The list is empty!</div>;
   }
+
+  <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
 
   return (
     <div>
