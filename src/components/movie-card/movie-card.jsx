41b3ee46247d6905fcd45d16fb4abc603e00eb64
Jelloko <rainbow.samp@gmail.com>
2024-10-16 02:05:17 -0400

    This commit should add Button and Card from react-bootstrap and remove previous layout to a Card layout for movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,14 +1,18 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movie);
-      }}
-    >
-      {movie.Title}
-    </div>
+    <Card className="h-100">
+      <Card.Img variant="top" src={movie.ImagePath} />
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>{movie.Director.Name}</Card.Text>
+        <Button onClick={() => onMovieClick(movie)} variant="link">
+          Open
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };  MovieCard.propTypes = {
     movie: PropTypes.shape({
@@ -16,7 +20,7 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       Title: PropTypes.string.isRequired,
       ImagePath: PropTypes.string.isRequired,
       Director: PropTypes.shape({
-        Name: PropTypes.string,
+        Name: PropTypes.string.isRequired
       })
     }).isRequired,
     onMovieClick: PropTypes.func.isRequired
