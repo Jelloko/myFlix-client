d31bdc6c23625fd441a712741d3e5c74a24495de
Jelloko <rainbow.samp@gmail.com>
2024-10-16 22:32:46 -0400

    Added import "bootstrap/dist/css/bootstrap.min.css";

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import { MainView } from './components/main-view/main-view';
 
 // Import statement to indicate that you need to bundle `./index.scss`
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 
 // Main component (will eventually use all the others)
