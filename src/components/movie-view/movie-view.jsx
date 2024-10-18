1d6d157f6c08130bb23f81bd02aad775bc1b0846
Jelloko <rainbow.samp@gmail.com>
2024-10-18 03:34:21 -0400

    This commit slightly changed the spelling for MovieId

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,10 +2,10 @@ import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import "./movie-view.scss";
 
-export const MovieView = ({ movie }) => {
-  const { movie_id } = useParams();
+export const MovieView = ({ movies }) => {
+  const { movieId } = useParams();
 
-  const movie = movie.find((b) => b.id === movie_id);
+  const movie = movies.find((b) => b.id === movieId);
 
 
   return (
