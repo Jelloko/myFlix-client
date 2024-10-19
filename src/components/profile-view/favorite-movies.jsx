8dda51664d2eb8fecfbb03928657f5f0ae0fa041
Jelloko <rainbow.samp@gmail.com>
2024-10-19 03:21:50 -0400

    This commit should create the favorite-movies page

---
+++ src/components/profile-view/favorite-movies.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Card, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export const FavoriteMovies = ({ favmov }) => {
+  return (
+    <div>
+      {favmov.length === 0 ? (
+        <p>No movies</p>
+      ) : (
+        favmov.map((movie) => (
+          <Card key={movie._id} className="mb-4">
+            <Card.Body>
+              <Card.Title>{movie.title}</Card.Title>
+              <Card.Text>{movie.director.name}</Card.Text>
+              <Link to={`/movies/${movie._id}`}>
+                <Button variant="primary">Movie Information</Button>
+              </Link>
+            </Card.Body>
+          </Card>
+        ))
+      )}
+    </div>
+  );
+};
+
+FavoriteMovies.prototype = {
+  FavoriteMovies: PropTypes.array.isRequired
+};
