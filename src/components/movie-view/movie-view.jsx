979f60c7585b0df4bf5b89e0a2677b5ece9484ff
Jelloko <rainbow.samp@gmail.com>
2024-10-19 03:21:27 -0400

    This commit should add the option to add or delete favorite movies

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,17 +1,69 @@
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import "./movie-view.scss";
 
-export const MovieView = ({ movies }) => {
+export const MovieView = ({ movies, user, token, setUser }) => {
   const { movieId } = useParams();
-
+  const [isFavorite, setIsFavorite] = useState(false);
   const movie = movies.find((b) => b.id === movieId);
 
+  useEffect(() => {
+    if(user && user.FavoriteMovies)  {
+        const isFavorite = user.FavoriteMovies.includes(movieId);
+        setIsFavorite(isFavorite);
+    }
+}, [movieId, user]);
 
-  return (
+const addtoFavorite = () => {
+    fetch(`https://my-flix-cf-fd6a3633859c.herokuapp.com/users${user.Name}/${movieId}`,
+    {
+        method: "POST",
+        headers: { 
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}` 
+        }
+    }).then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+    })
+    .then((data) => {
+        setUser(data);
+        localStorage.setItem("user", JSON.stringify(data));
+        setIsFavorite(true);
+    })
+    .catch((e) => {
+        console.log(e);
+    });       
+};
+const removefromFavorite = () => {
+    fetch(`https://my-flix-cf-fd6a3633859c.herokuapp.com/users/${user.Name}/${movieId}`,
+    {
+        method: "DELETE",
+        headers: { 
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}` 
+        }
+    }).then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+    })
+    .then((data) => {
+        setUser(data);
+        localStorage.setItem("user", JSON.stringify(data));
+        setIsFavorite(false);
+    })
+    .catch((e) => {
+    console.log(e);
+    });       
+};
+
+   return (
     <div>
       <div>
-        <img w-100 src={movie.ImagePath} />
+        <img src={movie.ImagePath} />
         </div>
       <div>
         <span>_id: </span>
@@ -32,6 +84,13 @@ export const MovieView = ({ movies }) => {
       <Link to={`/`}>
         <button className="back-button">Back</button>
       </Link>
+      <div className="mt-1"> 
+        {isFavorite ? (
+          <Button variant="danger" onClick={removefromFavorite}>Remove from favorite</Button>
+      ) : (
+          <Button variant="primary" onClick={addtoFavorite}>Add to favorite</Button>   
+      )}
+      </div>
     </div>
   );
 };
