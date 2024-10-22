ec7c681d0f92583261d42aed0a5084886ee5ad36
Jelloko <rainbow.samp@gmail.com>
2024-10-22 00:36:35 -0400

    Nothing

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -4,6 +4,7 @@ import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const MovieCard = ({ movie }) => {
+
   return (
     <Card className="h-100">
       <Card.Img variant="top" src={movie.ImagePath} />
@@ -12,7 +13,7 @@ export const MovieCard = ({ movie }) => {
         <Card.Text>{movie.Director.Name}</Card.Text>
         <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
           <Button variant="link">Open</Button>
-        </Link>
+          </Link>
       </Card.Body>
     </Card>
   );
