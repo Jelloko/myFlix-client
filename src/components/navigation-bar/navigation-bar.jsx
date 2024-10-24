60a6d8c09b0b5a69b4c7d064b58a41050cf13f9e
Jelloko <rainbow.samp@gmail.com>
2024-10-24 02:43:41 -0400

    Made changes to info button in movie-card

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar classname= 'nvbar' bg="orange" expand="lg">
       <Container>
         <Navbar.Brand as={Link} to="/">
           Myflix App
